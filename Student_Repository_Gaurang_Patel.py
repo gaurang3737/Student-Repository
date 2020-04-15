0e755af2413c1960a89f2351bcdce0f336d14b07
gaurang3737 <gaurangpatel3737@gmail.com>
2020-04-14 23:27:39 -0400

    Added Grade Summary
    Added Database functionality
    Added Grade Summary Pretty Table

--- Student_Repository_Gaurang_Patel.py
+++ Student_Repository_Gaurang_Patel.py
@@ -1,10 +1,7 @@
 """
-    Homework-10 : Modifying Homework-9 with new functionalities
-    1) Added Major class to support required and elective courses for each major
-    2) Added functionality to convert grades into score
-    3) Added functionality to calculate student GPA
-    4) Added new and modified pretty tables
-    5) Modified Student to support tracking of remaining required and elective courses of respected major
+    Homework-11 : Modifying Homework-10 with new functionalities
+    1) Added Database functionality to create Grade Summary
+    2) Added new pretty table for Grade Summary
 """
 
 from collections import defaultdict
@@ -12,9 +9,8 @@ from prettytable import PrettyTable
 from typing import Dict,DefaultDict,List,Iterator,Any,Tuple
 from HW08_Gaurang_Patel import file_reader
 import os
+import sqlite3
 
-#to convert grades into score
-SCORES:Dict[str,float] = {'A':4.0,'A-':3.75,'B+':3.25,'B':3.0,'B-':2.75,'C+':2.25,'C':2.0,'C-':0,'D+':0,'D':0,'D-':0,'F':0} 
 
 class Major:
     """stores information about majors"""
@@ -45,6 +41,8 @@ class Major:
 class Student:
     """"stores every information regarding a single student"""
     PT_FIELD_NAMES:List[str] = ['CWID','Name','Major','Completed Courses','Remaining Required','Remaining Electives','GPA'] #class attribute for student pretty table    
+    
+    SCORES:Dict[str,float] = {'A':4.0,'A-':3.75,'B+':3.25,'B':3.0,'B-':2.75,'C+':2.25,'C':2.0,'C-':0,'D+':0,'D':0,'D-':0,'F':0} #to convert grades into score
 
     def __init__(self, cwid:str, name:str, major:str, required:List[str], electives:List[str])-> None:
         """create instance of a Student class using cwid,name and major argument"""
@@ -54,12 +52,16 @@ class Student:
         self._courses:Dict[str,str] = dict() #courses['CS510'] = 'A'
         self._remaining_required:List[str] = required
         self._remaining_electives:List[str] = electives
-        self._gpa:float = 0.0 
+        self._gpa:float = 0.0
+        self._all_courses:Dict[str,str] = dict() #all_courses['SSW810'] = 'F' needed for GPA calculation
 
     def add_course_grade(self, course:str, grade:str)->None:
-        """adding course and grade to the 'courses' dictionary"""
+        """adding course and grade to the respective dictionary and calculating GPA"""
+        
+        #keep track of all courses taken by student
+        self._all_courses[course] = grade
 
-        #Tracking remaining required and electives
+        #Tracking remaining required and electives only if student passed the course
         if grade in ['A','A-','B+','B','B-','C+','C',]:
             self._courses[course] = grade
             if course in self._remaining_required:
@@ -69,7 +71,7 @@ class Student:
 
         #Calculating GPA
         if len(self._courses.values()) > 0 :
-            self._gpa = sum([SCORES[grade] for grade in self._courses.values()])/len(self._courses.values())
+            self._gpa = sum([Student.SCORES[grade] for grade in self._all_courses.values()])/len(self._all_courses.values())
 
     def info(self) -> Tuple[str,str,str,List[str],List[str],List[str],float]:
         """return a tuple with information about a student needed for the pretty table"""
@@ -117,24 +119,50 @@ class Repository:
             #for testing purpose
             self.student_data:List[Tuple[str,str,str,List[str],List[str],List[str],float]] = list() 
             self.instructor_data:List[Tuple[str,str,str,str,int]] = list() 
-            self.major_data:List[Tuple[str,List[str],List[str]]] = list() 
+            self.major_data:List[Tuple[str,List[str],List[str]]] = list()
+            self.grade_summary_data:List[Tuple[str,str,str,str,str]] = list() 
 
             #Generating pretty tables
             self._major_pt:PrettyTable = self.major_pretty_table()
             self._s_pt:PrettyTable = self.student_pretty_table()
             self._ins_pt:PrettyTable = self.instructor_pretty_table()
+            self._gs_pt:PrettyTable = self.grade_summary_pretty_table()
 
             #Print the pretty tables only if required
             if flag:
-                print(self._s_pt)
-                print(self._ins_pt)
+                print(f"\nMajor Summary")
                 print(self._major_pt)
+                print(f"\nStudent Summary")
+                print(self._s_pt)
+                print(f"\nInstructor Summary")
+                print(self._ins_pt)
+                print(f"\nStudent Grade Summary")
+                print(self._gs_pt)
+
         except FileNotFoundError as fe:
             print(fe) #Just Print the message
         except ValueError as ve:
             print(ve) #Just print the message
 
-            
+
+    def _grade_summary(self) -> Iterator[Tuple[str,str,str,str,str]]:
+        """Generator for reading data from database located at given path"""
+        try:
+            db:sqlite3.Connection = sqlite3.connect(os.path.join(self._path,"hw11.db"))
+        except sqlite3.OperationalError as e:
+                print(e)
+        else:
+            try:
+                query:str = """select s.Name,s.CWID,g.Course,g.Grade,i.Name
+                            from grades g join students s on g.StudentCWID = s.CWID
+                            join instructors i on g.InstructorCWID = i.CWID
+                            order by s.Name
+                            """
+                for tup in db.execute(query):
+                    yield tup
+            except sqlite3.OperationalError as e:
+                print(e)
+
     def _read_majors(self) -> None:
         """reading major.txt and updating the 'majors' dictionary"""
         for major,category,course_name in file_reader(os.path.join(self._path,"majors.txt"),3,sep='\t',header=True):
@@ -144,7 +172,7 @@ class Repository:
 
     def _read_students(self) -> None:
         """reading students.txt and updating the 'students' dictionary"""
-        for cwid,name,major in file_reader(os.path.join(self._path,"students.txt"),3,sep=';',header=True):
+        for cwid,name,major in file_reader(os.path.join(self._path,"students.txt"),3,sep='\t',header=True):
             if self._majors.get(major) is None:#Student with major not found in major.txt
                 self._students[cwid] = Student(cwid,name,major,[],[])
             else:
@@ -152,13 +180,13 @@ class Repository:
 
     def _read_instructors(self) -> None:
         """reading instructors.txt and updating the 'instructors' dictionary"""
-        for cwid,name,dept in file_reader(os.path.join(self._path,"instructors.txt"),3,sep='|',header=True):
+        for cwid,name,dept in file_reader(os.path.join(self._path,"instructors.txt"),3,sep='\t',header=True):
             self._instructors[cwid] = Instructor(cwid,name,dept)
 
     def _read_grades(self) -> None:
         """reading grades.txt and updating respective instances of Student and Instructor class"""
         for student_cwid,course_name,grade,instructor_cwid in \
-        file_reader(os.path.join(self._path,"grades.txt"),4,sep='|',header=True):
+        file_reader(os.path.join(self._path,"grades.txt"),4,sep='\t',header=True):
             if self._students.get(student_cwid) is None: #Unknown Student
                 print(f"Student with CWID = {student_cwid} does not exists!")
             elif self._instructors.get(instructor_cwid) is None: #Unknown Instructor
@@ -170,7 +198,7 @@ class Repository:
                 inst.store_course_students(course_name)
 
     def student_pretty_table(self) -> PrettyTable:
-        """For generating and printing the Student pretty table"""
+        """For generating the Student pretty table"""
         s_pt:PrettyTable = PrettyTable(field_names=Student.PT_FIELD_NAMES)
         for student in self._students.values():
             s_pt.add_row(student.info())
@@ -178,7 +206,7 @@ class Repository:
         return s_pt
 
     def instructor_pretty_table(self) -> PrettyTable:
-        """For generating and printing the Instructor pretty table"""
+        """For generating the Instructor pretty table"""
         ins_pt:PrettyTable = PrettyTable(field_names=Instructor.PT_FIELD_NAMES)
         for instructor in self._instructors.values():
             for row in list(instructor.info()):
@@ -187,21 +215,25 @@ class Repository:
         return ins_pt
 
     def major_pretty_table(self) -> PrettyTable:
-        """For generating and printing Major pretty table"""
+        """For generating Major pretty table"""
         major_pt:PrettyTable = PrettyTable(field_names=Major.PT_FIELD_NAMES)
         for major in self._majors.values():
             major_pt.add_row(major.info())
             self.major_data.append(major.info())
         return major_pt
 
+    def grade_summary_pretty_table(self) -> PrettyTable:
+        """For generating grade summary pretty table"""
+        gs_pt:PrettyTable = PrettyTable(field_names=['Name','CWID','Course','Grade','Instructor'])
+        for row in list(self._grade_summary()):
+            gs_pt.add_row(row)
+            self.grade_summary_data.append(row)
+        return gs_pt
+
 
 def main() -> None:
     """define two Repositories, one for Stevens and one for NYU"""
-    empty:Repository = Repository(r"C:\Users\user\Desktop\StevensSem4\SSW-810 Python\HW10-Repository\test_empty",True)
-    not_exists:Repository = Repository(r"C:\Users\user\Desktop\StevensSem4\SSW-810 Python\HW10-Repository\not_exists",True)
-    fail:Repository = Repository(r"C:\Users\user\Desktop\StevensSem4\SSW-810 Python\HW10-Repository\test_fail")
-    print(f"\n--------------------Correct Output----------------------\n")
-    stevens:Repository = Repository(r"C:\Users\user\Desktop\StevensSem4\SSW-810 Python\HW10-Repository\test_hw10",True)  
+    stevens:Repository = Repository(r"C:\Users\user\Desktop\StevensSem4\SSW-810 Python\HW11-Repository\test_hw11",True)  
 
 if __name__ == "__main__":
     main() 
